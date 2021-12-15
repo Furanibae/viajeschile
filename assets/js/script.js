222e16b983ea806f63f8af7debe139f31203577e
furanibae <fran.isa@live.com>
2021-12-15 15:18:04 -0300

    Todo Terminado

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// tootip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+// correo
+
+  document.getElementById("enviarCorreo").addEventListener("click", function() {
+    alert("Gracias por contactarse con nosotros."); });
+
+
+
